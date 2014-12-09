57b490358beba76ed2ca115b816f32ddfa3ce6ac
Gareth George <garethgeorge97@gmail.com>
2014-12-09 12:13:44 -0800

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Gareth George on 12/9/14.
+//  Copyright (c) 2014 Gareth George. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
